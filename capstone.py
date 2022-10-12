3f59cc5e0da7115fcd9cf71c288d095d9f5a9048
hkuswari <115609882+hkuswari@users.noreply.github.com>
2022-10-12 15:11:47 +0700

    Update capstone.py

--- capstone.py
+++ capstone.py
@@ -2,9 +2,14 @@ import streamlit as st
 import pandas as pd
 
 df_inf = pd.read_csv("inflasiyoy.csv", sep=";", parse_dates=['Bulan'], index_col=['Bulan'])
+df_tpt = pd.read_csv("tpt.csv", sep=";", parse_dates=['Tahun'], index_col=['Tahun'])
 
 st.title("Inflasi dan Tingkat Pengangguran Terbuka di Indonesia")
 st.write("Saat ini inflasi di Indonesia sedang mengalami peningkatan. Pada bulan September tahun 2022\
-    tingkat inflasi IHK di Indonesia mencapai 5,95%.")
-st.line_chart(df_inf)
+    tingkat inflasi IHK di Indonesia mencapai 5,95%. Berdasarkan hal ini maka akan dilihat hubungan dari\
+    tingkat Inflasi dan Tingkat Pengangguran Terbuka (TPT) di Indonesia. ")
+
 st.caption("Sumber: Bank Indonesia")
+st.line_chart(df_inf)
+st.caption("Sumber: BPS")
+st.line_chart(df_tpt)
